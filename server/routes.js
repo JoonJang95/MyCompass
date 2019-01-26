6cdd5cb775629c6032a7b73737069aaf60bca3ec
joon jang <joonjang95@gmail.com>
2019-01-26 15:47:14 -0500

    Set up initial file structure

---
+++ server/routes.js
@@ -0,0 +1,9 @@
+const controller = require('./controller');
+const router = require('express').Router();
+
+router.get();
+router.post();
+router.put();
+router.delete();
+
+module.exports = router;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './client/index.jsx',
+  output: {
+    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, './public')
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [new webpack.optimize.ModuleConcatenationPlugin()],
+  node: {
+    fs: 'empty'
+  }
+};
