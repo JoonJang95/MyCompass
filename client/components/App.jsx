cfe0c04123ebc9b9aa9b5f697f1349f0cbbbc56d
joon jang <joonjang95@gmail.com>
2019-01-27 17:53:09 -0500

    Initialize map react components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import Login from './Login.jsx';
 import Questions from './Questions.jsx';
-import Map from './Map.jsx';
+import RecommendationsMap from './RecommendationsMap.jsx';
+import Recommendations from './Recommendations.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      mapRegion: null,
+      gpsAccuracy: null,
+      recommendations: [],
+      lookingFor: null,
+      headerlocation: null,
+      last4sqCall: null
+    };
+  }
+
+  getUserLocation() {
+    navigator.geolocation.watchPosition(position => {
+      let region = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: 0.00922 * 1.5,
+        longitudeDelta: 0.00421 * 1.5
+      };
+    });
+  }
+
+  onLocationChange(region, gpsAccuracy) {
+    this.setState({
+      mapRegion: region,
+      gpsAccuracy: gpsAccuracy || this.state.gpsAccuracy
+    });
   }
 
   componentDidMount() {
@@ -20,7 +46,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Map />
+        <RecommendationsMap />
       </div>
     );
   }
---
+++ client/components/Recommendations.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Recommendations = () => {
+  return (
+    <div>
+      <div>Recommendations Page</div>
+    </div>
+  );
+};
+
+export default Recommendations;
