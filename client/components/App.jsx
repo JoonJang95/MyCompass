6e42a671f52301f9139f2d16e6f70230da4ecaf9
joon jang <joonjang95@gmail.com>
2019-01-28 01:25:37 -0500

    Begin setting up FourSquare Api call

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import axios from 'axios';
 import Login from './Login.jsx';
 import Map from './Map.jsx';
+import { FourSquareID, FourSquareSecret } from '../../envConfigs.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -8,7 +10,9 @@ class App extends React.Component {
 
     this.state = {
       longitude: -73.968285,
-      latitude: 40.785091
+      latitude: 40.785091,
+      gpsAccuracy: 30,
+      lookingFor: 'food'
     };
   }
 
@@ -19,15 +23,31 @@ class App extends React.Component {
   getLocation() {
     navigator.geolocation.getCurrentPosition(
       pos => {
+        console.log('hey', pos);
         this.setState({
           longitude: pos.coords.longitude,
-          latitude: pos.coords.latitude
+          latitude: pos.coords.latitude,
+          gpsAccuracy: pos.coords.accuracy
         });
       },
       err => {
         console.log('Could not get current location: ', err);
-      }
+      },
+      { enableHighAccuracy: true }
     );
+  }
+
+  getNearbyRecommendations() {
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore';
+    const parameters = {
+      client_id: FourSquareID,
+      client_secret: FourSquareSecret,
+      section: this.state.lookingFor,
+      ll: `${this.state.latitude}, ${this.state.longitude}`,
+      llAcc: this.state.gpsAccuracy,
+      limit: 10,
+      openNow: 1
+    };
   }
 
   render() {
--- envConfigs.js
+++ envConfigs.js
@@ -3,5 +3,7 @@ dotenv.config();
 
 module.exports = {
   MB_APIKEY: process.env.MBAPI_KEY,
-  PORT: process.env.PORT
+  PORT: process.env.PORT,
+  FourSquareID: process.env.FourSquareID,
+  FourSquareSecret: process.env.FourSquareSecret
 };
