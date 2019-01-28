84746755263db8166f8a3de8477e855d9653db9d
joon jang <joonjang95@gmail.com>
2019-01-28 02:51:21 -0500

    Implement successful Api call to 4Square

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -12,7 +12,8 @@ class App extends React.Component {
       longitude: -73.968285,
       latitude: 40.785091,
       gpsAccuracy: 30,
-      lookingFor: 'food'
+      lookingFor: 'food',
+      recommendations: []
     };
   }
 
@@ -23,12 +24,16 @@ class App extends React.Component {
   getLocation() {
     navigator.geolocation.getCurrentPosition(
       pos => {
-        console.log('hey', pos);
-        this.setState({
-          longitude: pos.coords.longitude,
-          latitude: pos.coords.latitude,
-          gpsAccuracy: pos.coords.accuracy
-        });
+        this.setState(
+          {
+            longitude: pos.coords.longitude,
+            latitude: pos.coords.latitude,
+            gpsAccuracy: pos.coords.accuracy
+          },
+          () => {
+            this.getNearbyRecommendations();
+          }
+        );
       },
       err => {
         console.log('Could not get current location: ', err);
@@ -38,16 +43,36 @@ class App extends React.Component {
   }
 
   getNearbyRecommendations() {
-    const endPoint = 'https://api.foursquare.com/v2/venues/explore';
+    console.log('new hey call');
+    const endPoint = 'https://api.foursquare.com/v2/search/recommendations?';
     const parameters = {
       client_id: FourSquareID,
       client_secret: FourSquareSecret,
-      section: this.state.lookingFor,
       ll: `${this.state.latitude}, ${this.state.longitude}`,
       llAcc: this.state.gpsAccuracy,
+      intent: this.state.lookingFor,
       limit: 10,
-      openNow: 1
+      openNow: true,
+      v: '20190123'
     };
+
+    axios
+      .get(endPoint + new URLSearchParams(parameters))
+      .then(({ data }) => {
+        const { results } = data.response.group;
+        console.log('data from 4square', results);
+        this.setState(
+          {
+            recommendations: results
+          },
+          () => {
+            console.log('new state rec', this.state.recommendations);
+          }
+        );
+      })
+      .catch(err => {
+        console.log('error with get nearby recommendations request: ', err);
+      });
   }
 
   render() {
