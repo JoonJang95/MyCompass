af1f1f81e6c4f26af5d1eb4cab81cb80a1fcf6db
joon jang <joonjang95@gmail.com>
2019-01-29 03:20:52 -0500

    Begin adding a login authentication feature

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,29 +1,78 @@
 import React from 'react';
 import auth from './Auth.js';
+import axios from 'axios';
 
-const Login = props => {
-  return (
-    <div>
-      <form className="loginForm">
-        <h1 className="loginHeader">MyCompass App</h1>
-        <input type="text" className="login-input" placeholder="username" />
-        <input type="password" className="login-input" placeholder="password" />
-        <button
-          onClick={e => {
-            e.preventDefault();
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
 
-            auth.login(() => {
-              props.history.push('/');
-            });
-          }}
-          className="login-button"
-        >
-          Login
-        </button>
-        <p className="login-forgot">Forgot Password?</p>
-      </form>
-    </div>
-  );
-};
+    this.state = {
+      username: '',
+      password: ''
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.submitLogin = this.submitLogin.bind(this);
+  }
+
+  onInputChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  submitLogin() {
+    axios
+      .post('/login', {
+        username: this.state.username,
+        password: this.state.password
+      })
+      .then(({ data }) => {
+        if (results.data.valid) {
+          auth.login(() => {
+            props.history.push('/');
+          });
+        } else {
+          alert('Wrong Username or Password');
+        }
+      })
+      .catch(err => {
+        console.log('error checking credentials: ', err);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="loginForm">
+          <h1 className="loginHeader">MyCompass App</h1>
+          <input
+            name="username"
+            onChange={this.onInputChange}
+            type="text"
+            className="login-input"
+            placeholder="username"
+          />
+          <input
+            name="password"
+            onChange={this.onInputChange}
+            type="password"
+            className="login-input"
+            placeholder="password"
+          />
+          <button
+            onClick={e => {
+              e.preventDefault();
+            }}
+            className="login-button"
+          >
+            Login
+          </button>
+          <p className="login-forgot">Forgot Password?</p>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Login;
